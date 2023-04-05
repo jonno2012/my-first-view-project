90c9835c6d317e638d0b9b5a75cf45a7acb2c8ae
jonno2012 <jonathanrhill78@gmail.com>
2023-04-05 17:47:43 +0100

    The composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+// import { ref } from "vue"
+
+let message = $ref('test')
+
+let doSomething = () => {
+  alert("sfefsdfsdf")
+}
+
+setTimeout((() => {
+  message.value = 'i have been changed'
+}), 2000)
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message">
+      <br>
+      <button @click="doSomething">Do Something</button>
+    </p>
   </main>
 </template>
