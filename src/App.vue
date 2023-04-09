7407a7e6973d7bf23661634e653a8ef9dbb7e8d1
jonno2012 <jonathanrhill78@gmail.com>
2023-04-09 17:23:36 +0100

    Code Organisation

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/contact">Contact</RouterLink>
         <RouterLink to="/counter">Counter</RouterLink>
+        <RouterLink to="/team">Team</RouterLink>
       </nav>
     </div>
   </header>
