a44efe09125055575b7a95ea13b4735899750919
jonno2012 <jonathanrhill78@gmail.com>
2023-04-09 20:52:10 +0100

    Teleporting

--- src/components/Teams/TeamFooter.vue
+++ src/components/Teams/TeamFooter.vue
@@ -1,6 +1,8 @@
 <template>
   <footer class="mt-12 bg-gray-100 py-4 text-center">
     <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spots }} Member Team</h5>
+
+    <div id="hello"></div>
   </footer>
 </template>
 
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,20 +2,18 @@
 import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
-import Modal from "@/components/Modal.vue";
-import {ref} from "vue";
 import {useTeamStore} from "@/stores/TeamStore";
 
 let team = useTeamStore()
 
 team.fill()
 
-let showModal = ref(false)
+
 
 </script>
 
 <template>
-  <TeamHeader @add="showModal = true"/>
+  <TeamHeader/>
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
     <TeamMembers/>
@@ -23,18 +21,4 @@ let showModal = ref(false)
 
   <TeamFooter/>
 
-  <Modal :show="showModal" @close="showModal = false">
-    <template #header>Override header</template>
-    <template #default>
-      <p>Need to add a new member to your team</p>
-
-      <form class="mt-6">
-        <div class="flex gap-2">
-          <input type="email" placeholder="Email Address..." class="flex-1 p-1">
-          <button>Add</button>
-        </div>
-      </form>
-    </template>
-<!--    <template #footer>Override footer</template>-->
-  </Modal>
 </template>
